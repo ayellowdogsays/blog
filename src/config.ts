a40b6c1d22124277f7253593630f7eee558f730a
Your Name <you@example.com>
2025-08-29 14:54:57 +0800

    first commit

---
+++ src/config.ts
@@ -0,0 +1,138 @@
+import type { Site, Ui, Features } from './types'
+
+export const SITE: Site = {
+  website: 'https://astro-antfustyle-theme.vercel.app/',
+  base: '/',
+  title: '阿黄',
+  description:
+    '你好👋我是阿黄说',
+  author: 'Mr Dog',
+  lang: 'zh',
+  ogLocale: 'ZH-CN',
+  imageDomains: ['cdn.bsky.app', 'images.unsplash.com'],
+}
+
+export const UI: Ui = {
+  internalNavs: [
+    {
+      path: '/blog',
+      title: '博文',
+      displayMode: 'alwaysText',
+      text: '博文',
+    },
+    {
+      path: '/changelog',
+      title: 'Changelog',
+      displayMode: 'iconToTextOnMobile',
+      text: 'Changelog',
+      icon: 'i-ri-draft-line',
+    },
+  ],
+  socialLinks: [
+    {
+      link: 'https://github.com/ayellowdogsays',
+      title: '阿黄',
+      displayMode: 'alwaysIcon',
+      icon: 'i-uil-github-alt',
+    },
+  ],
+  navBarLayout: {
+    left: [],
+    right: [
+      'internalNavs',
+      'hr',
+      'socialLinks',
+      'hr',
+      'themeButton',
+      'rssLink',
+    ],
+    mergeOnMobile: true,
+  },
+  tabbedLayoutTabs: [
+    { title: 'Changelog', path: '/changelog' },
+    { title: 'AstroBlog', path: '/feeds' },
+    { title: 'AstroStreams', path: '/streams' },
+  ],
+  groupView: {
+    maxGroupColumns: 3,
+    showGroupItemColorOnHover: true,
+  },
+  githubView: {
+    monorepos: [
+      'withastro/astro',
+      'withastro/starlight',
+      'lin-stephanie/astro-loaders',
+    ],
+    mainLogoOverrides: [
+      [/starlight/, 'https://starlight.astro.build/favicon.svg'],
+    ],
+    subLogoMatches: [
+      [/theme/, 'i-unjs-theme-colors'],
+      [/github/, 'https://github.githubassets.com/favicons/favicon.svg'],
+      [/tweet/, 'i-logos-twitter'],
+      [/bluesky/, 'i-logos-bluesky'],
+    ],
+  },
+  externalLink: {
+    newTab: false,
+    cursorType: '',
+    showNewTabIcon: false,
+  },
+  postMetaStyle: 'minimal',
+}
+
+/**
+ * Configures whether to enable special features:
+ *  - Set to `false` or `[false, {...}]` to disable the feature.
+ *  - Set to `[true, {...}]` to enable and configure the feature.
+ */
+export const FEATURES: Features = {
+  slideEnterAnim: [true, { enterStep: 60 }],
+  ogImage: [
+    true,
+    {
+      authorOrBrand: `${SITE.title}`,
+      fallbackTitle: `${SITE.description}`,
+      fallbackBgType: 'plum',
+    },
+  ],
+  toc: [
+    true,
+    {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 4,
+      displayPosition: 'left',
+      displayMode: 'content',
+    },
+  ],
+  share: [
+    true,
+    {
+      twitter: [true, '@ste7lin'],
+      bluesky: [true, '@ste7lin.bsky.social'],
+      mastodon: false,
+      facebook: false,
+      pinterest: false,
+      reddit: false,
+      telegram: false,
+      whatsapp: false,
+      email: false,
+    },
+  ],
+  giscus: [
+    true,
+    {
+      'data-repo': 'ayellowdogsays/giscus',
+      'data-repo-id': 'R_kgDOPk4m3A',
+      'data-category': 'Announcements',
+      'data-category-id': 'DIC_kwDOPk4m3M4CupDE',
+      'data-mapping': 'pathname',
+      'data-strict': '0',
+      'data-reactions-enabled': '1',
+      'data-emit-metadata': '0',
+      'data-input-position': 'bottom',
+      'data-theme': 'fro',
+      'data-lang': 'zh-CN',
+    },
+  ],
+}
---
+++ src/content/home/index.md
@@ -0,0 +1,9 @@
+### 星星应该哈哈大笑，反正宇宙是个偏僻的地方，也许我站在你看不到的地方，看了你好多次
+<img src="https://ghchart.rshah.org/ayellowdogsays" />
+<div class="flex flex-wrap gap-4">
+  <a class="btn-rose" href="https://github.com/ayellowdogsays">
+    <span class="i-ph-heart-duotone"></span>跟我走</a>
+  <a class="btn-yellow" href="/blog">
+    <span class="i-ph-lightning-duotone"></span>点我看最近写的文章（原博客）</a>
+</div>
+
